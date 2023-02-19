9b10e5de7ee8f14ffa87e5de837aa86af683d797
Alina Eksta <124583566+alinaeksta@users.noreply.github.com>
2023-02-19 16:49:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 # python3
-
 from collections import namedtuple
 import requests
 
+
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
+    return (left + right) in ["()", "{}", "[]"]
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i-1))
-
+            opening_brackets_stack.append(Bracket(next, i - 1 ))
         if next in ")]}":
-            if not opening_brackets_stack: 
-                return i+1
+            if not opening_brackets_stack:
+                return i + 1 
             if not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+                return i + 1
+            
             opening_brackets_stack.pop()
-        if opening_brackets_stack:
-          return opening_brackets_stack[-1].position
-        return "Success"
-
+    
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position 
+    return "Success"
 
 def main():
+
     test_url = "https://github.com/DA-testa/steks-un-iekavas-ArtemijsRadionovs/blob/main/test/0"
     check_for_I = input()
     if check_for_I.startswith("I"):
@@ -39,8 +37,5 @@ def main():
         print("Success")
     else:
         print(find_mismatch(text))
-
-
-
 if __name__ == "__main__":
     main()
