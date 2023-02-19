edaa839891704387a2c0728cbe05ebbcc9c48654
Alina Eksta <124583566+alinaeksta@users.noreply.github.com>
2023-02-19 16:45:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next,i-1))
 
         if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-             return i+1
+            if not opening_brackets_stack: 
+                return i+1
+            are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
             opening_brackets_stack.pop()
         if opening_brackets_stack:
           return opening_brackets_stack[-1].position
