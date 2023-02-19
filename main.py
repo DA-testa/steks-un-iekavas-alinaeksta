5f940b2d188de7666aaabbec3963feddea0270f9
Alina Eksta <124583566+alinaeksta@users.noreply.github.com>
2023-02-19 16:38:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
+            opening_brackets_stack.append(Bracket(next,i-1))
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
@@ -25,20 +25,12 @@ def find_mismatch(text):
 
 
 def main():
-    check = input()
-    if check == "I":
+    #check = input()
+    #if check == "I":
     
-        text = input()
-        mismatch = find_mismatch(text)
-        if mismatch == -2:
-            for i, next in enumerate(text):
-                if (i>=1999):
-                    print("972")
-                else :
-                    print(4)
-        
-        else:
-            print(mismatch)
+    text = input()
+    mismatch = find_mismatch(text)
+    print(mismatch)
         
     #else:
         #mismatch = find_mismatch(text)
